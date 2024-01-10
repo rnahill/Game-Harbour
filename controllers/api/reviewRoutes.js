68578b781646eb97dce7f36a2896ba1b0aee45ef
Alphonse Kusluch <alkusluch@gmail.com>
2024-01-10 18:49:09 -0500

    add route for search.handlebars to homeroutes and added and update to review routes

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -13,7 +13,22 @@ router.post('/addReview', withAuth, async (req, res) => {
     } catch (err) {
       res.status(400).json(err);
     }
-  });
+});
+
+router.put('/review/:id', withAuth, async (req, res) => {
+  try {
+    const updatedReview = await Review.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    res.status(200).json(updatedReview);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 
 
 module.exports = router;
